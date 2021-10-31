587e21dfeed836246d012bb1ffa2ecbd658dc265
Anan014 <ananamer95@gmail.com>
2021-10-31 12:28:07 +0200

    hr-cars done

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="ui segment">
+
+                <img class="ui fluid image" src="https://source.unsplash.com/1600x450/?car" />
+            <div className="ui segment">
+
+            </div>
+
+
         </div>
     )
 };
